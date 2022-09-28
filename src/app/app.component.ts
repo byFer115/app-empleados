188a542294d61d390a9e759be02d5a8ba2428eb5
Fernando Hernandez <fernando.hernandez@solsersistem.net>
2022-09-28 18:58:22 -0500

    Firt Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Empleado } from './empleado.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Lista de empleados';
+
+  empleados:Empleado[]=[
+      new Empleado("Juan","Diaz", "Presidente", 7500),
+      new Empleado("Luis","Diaz", "Directo", 5500),
+      new Empleado("Fernando","Diaz", "Administrativo", 4500),
+      new Empleado("Miguel","Diaz", "Presidente", 7500),
+  ];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  agregarEmpleado(){
+
+  }
 }
