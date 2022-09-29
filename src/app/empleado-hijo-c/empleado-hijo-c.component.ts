0ff8a99e3c01ee9d68c2cd03e073715d032d7cdb
Fernando Hernandez <fernando.hernandez@solsersistem.net>
2022-09-29 11:06:33 -0500

    Component Interaction @Input

---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,20 @@
+import { Empleado } from './../empleado.model';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent implements OnInit {
+  @Input() empleado:Empleado;
+  @Input() i:number;
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
